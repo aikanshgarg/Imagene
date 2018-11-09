3db9a2338674c8e90a7726069aa391ac75b7456c
root <aikanshgarg0303@gmail.com>
2018-11-10 01:31:17 +0530

    completed basic gallery

--- rGallery/rgallery/app/controllers/application_controller.rb
+++ rGallery/rgallery/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	before_action :getCategoryNav
+
+	private
+	def getCategoryNav
+		@categoryNav = Category.all		
+	end
+
+
 end
--- rGallery/rgallery/app/controllers/categories_controller.rb
+++ rGallery/rgallery/app/controllers/categories_controller.rb
@@ -1,19 +1,56 @@
 class CategoriesController < ApplicationController
+  
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category created ..."
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+# edit and show methods have same working as both are used in the same view
   def edit
+    @category = Category.find(params[:id])
+    @categories = Category.all
+    @images = @category.images
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = "Category updated ..."
+      redirect_to category_path(params[:id])
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    flash[:notice] = "Category Removed"
+    redirect_to root_path
   end
 
   def show
+    @category = Category.find(params[:id])
+    @categories = Category.all
+    @images = @category.images
   end
+
+
+  private
+  def category_params
+    params.require(:category).permit(:name)       
+  end
+
 end
